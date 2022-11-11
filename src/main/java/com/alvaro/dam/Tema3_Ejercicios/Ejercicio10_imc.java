c913aebe489120099e3382a8cc83c16f5377e313
10.159 <10.159@D13-PC01.amav.net>
2022-11-11 19:29:07 +0100

    modificado ejercicio6 y ejercicio 10

--- src/main/java/com/alvaro/dam/Tema3_Ejercicios/Ejercicio10_imc.java
+++ src/main/java/com/alvaro/dam/Tema3_Ejercicios/Ejercicio10_imc.java
@@ -15,9 +15,18 @@ public class Ejercicio10_imc {
 		System.out.println("Introduce la altura");
 		altura = entrada.nextFloat();
 		
-		 imc = peso/altura;
+		 imc = peso/(altura)2;
 		
 		System.out.println("IMC:" + imc);
-	}
 
+	if(imc<18.5) {
+		System.out.println("Peso inferior al normal");
+	} else if (imc>=18.5 & imc<24.9){
+		System.out.println("Peso normal");
+	} else if (imc>= 25.0 & imc<29.9) {
+		System.out.println("Peso superior al normal");
+	} else if (imc>30) {
+			System.out.println("Tienes obesidad");
+		} 
+	}
 }
--- src/main/java/com/alvaro/dam/Tema3_Ejercicios/Ejercicio6_mes.java
+++ src/main/java/com/alvaro/dam/Tema3_Ejercicios/Ejercicio6_mes.java
@@ -12,46 +12,18 @@ public class Ejercicio6_mes {
 		
 		
 		switch (mes) {
-		case 1:
-			System.out.println("Enero " + " 31 días");
+		case 1, 3, 5, 7, 8, 10, 12:
+			System.out.println("Tiene 31 días");
 			break;
 		case 2:
-			System.out.println("Febrero " + " 28 días");
+			System.out.println("Tiene 28 días");
 			break;
-		case 3:
-			System.out.println("Marzo " + " 31 días");
-			break;
-		case 4:
-			System.out.println("Abril" + " 30 días");
-			break;
-		case 5:
-			System.out.println("Mayo" + " 31 días");
-			break;
-		case 6:
-			System.out.println("Junio" + " 30 días");
-			break;
-		case 7:
-			System.out.println("Julio" + " 31 días");
-			break;
-		case 8:
-			System.out.println("Agosto" + " 31 días");
-			break;
-		case 9:
-			System.out.println("Septiembre" + " 30 días");
-			break;
-		case 10:
-			System.out.println("Octubre" + " 31 días");
-			break;
-		case 11:
-			System.out.println("Noviembre" + " 30 días");
-			break;
-		case 12:
-			System.out.println("Diciembre" + " 31 días");
+		case 4, 6, 9, 11:
+			System.out.println("Tiene 30 días");
 			break;
 			default:
 				System.out.println("Error tienes que introducir un número entre el 1 y 12");
-				
 		}
-	}	
+	}
 }
 
