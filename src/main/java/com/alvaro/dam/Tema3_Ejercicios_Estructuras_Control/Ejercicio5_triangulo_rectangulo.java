8c103b92ca1e9371702873d2716be1e2d60bc3d8
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-16 20:48:47 +0100

    subiendo ejercicio 5 6

--- src/main/java/com/alvaro/dam/Tema3_Ejercicios_Estructuras_Control/Ejercicio5_triangulo_rectangulo.java
+++ src/main/java/com/alvaro/dam/Tema3_Ejercicios_Estructuras_Control/Ejercicio5_triangulo_rectangulo.java
@@ -1,5 +1,28 @@
 package com.alvaro.dam.Tema3_Ejercicios_Estructuras_Control;
+import java.util.Scanner;
 
 public class Ejercicio5_triangulo_rectangulo {
-
+	public static void main(String[] args) {
+		  
+		int asteriscos = 1;
+	    int espacios = ancho - 1;
+	    int ancho;
+	    
+	    Scanner entrada = new Scanner(System.in);
+	    
+	    System.out.println("Dime el ancho del triangulo");
+	    ancho = entrada.nextInt();
+	    
+	        for (int fila = 0; fila < ancho; fila++) {
+	            for (int columnaEspacios = 0; columnaEspacios < espacios; columnaEspacios++)  {
+	                System.out.print(" ");
+	            }
+	            for (int columnaAsteriscos = 0; columnaAsteriscos < asteriscos; columnaAsteriscos++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	            
+	            asteriscos ++;
+	            espacios --;
+	            
 }
---
+++ src/main/java/com/alvaro/dam/Tema3_Ejercicios_Estructuras_Control/Ejercicio6_menu.java
@@ -0,0 +1,82 @@
+package com.alvaro.dam.Tema3_Ejercicios_Estructuras_Control;
+import java.util.Scanner;
+
+public class Ejercicio6_menu {
+	
+	public static void main(String[] args) {
+		int opcion;
+		
+		System.out.println("Elige una de las siguientes opciones");
+		System.out.println("1-) Tabla de multiplicar");
+		System.out.println("2-) Producto por sumas");
+		System.out.println("3-) Mostrar numeros comprendidos");
+		System.out.println("4-) Salir");
+		
+		Scanner entrada = new Scanner(System.in);
+		opcion = entrada.nextInt();
+		
+		switch(opcion){
+		case 1:
+			System.out.println("Tabla de multiplicar");
+			int num = 0, i, tabla = 0;
+			
+			Scanner entradaMultiplicar = new Scanner(System.in);
+			
+			System.out.println("Introduce un numero del 1 al 10");
+			num = entradaMultiplicar.nextInt();
+			
+			System.out.println("La tabla del " + num);
+			
+			if(num <= 10) {
+				
+				for(i=1; i<=10; i++) {
+					tabla = num * i;
+					System.out.println(num + " X " + i + " = " + tabla);
+				}
+				
+			} else {
+				System.out.println("Los numeros tienen que estar entre el 1 y el 10");
+			}
+			break;
+		case 2:
+			System.out.println("Productos por sumas");
+			
+			int num1, num2, suma;
+			
+			Scanner entradaSuma = new Scanner(System.in);
+			System.out.println("Introduce el primer numero");
+			num1 = entradaSuma.nextInt();
+			System.out.println("Introduce el segundo numero");
+			num2 = entradaSuma.nextInt();
+			
+			suma = num1 + num2;
+			
+			System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma);
+			break;
+		case 3:
+			System.out.println("Mostrar numeros comprendidos");
+			
+			int nume1, nume2;
+			
+			Scanner entradaComprendidos = new Scanner(System.in);
+			System.out.println("Introduce el primer numero");
+			nume1 = entradaComprendidos.nextInt();
+			System.out.println("Introduce el segundo numero");
+			nume2 = entradaComprendidos.nextInt();
+			
+			while (nume1 >= nume2) {
+				System.out.println
+			}
+			
+			
+			
+			break;
+		case 4:
+			System.out.println("Hasta pronto.......");
+			break;
+			
+		}
+		
+	}
+	
+}
