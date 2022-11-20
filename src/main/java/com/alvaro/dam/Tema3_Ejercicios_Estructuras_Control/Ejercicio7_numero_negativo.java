30b87955c1735d053970ae3a4232c7ce62f4e7cb
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-20 20:16:28 +0100

    subido ejercicio 7

---
+++ src/main/java/com/alvaro/dam/Tema3_Ejercicios_Estructuras_Control/Ejercicio7_numero_negativo.java
@@ -0,0 +1,32 @@
+package com.alvaro.dam.Tema3_Ejercicios_Estructuras_Control;
+import java.util.Scanner;
+
+public class Ejercicio7_numero_negativo {
+	
+	public static void main(String[] args) {
+		
+		int num=0;
+		int numAnterior=0;
+		int salida = 0;
+		
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		do {
+			System.out.println("Introduce un numero");
+			num = entrada.nextInt();
+			
+			if(num == numAnterior) {
+				numAnterior = num;
+				salida = salida + 1;
+			}
+		} while (num > 0);
+		
+	 if (salida>0) {
+		 System.out.println("Si");
+	 } else {
+		 System.out.println("No");
+	 }
+	}
+}
